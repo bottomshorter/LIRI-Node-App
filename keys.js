3fcaa658f256a4543dda2ab02ba057f27920f1b5
bottomshorter <longtopsshortbottoms@gmail.com>
2019-06-11 14:38:12 -0700

    OMDB functional

--- keys.js
+++ keys.js
@@ -5,3 +5,10 @@ exports.spotify = {
 
 console.log("Spotify ID and Secret linked.");
 console.log("-----------------------------------");
+
+exports.omdb = {
+  apikey: process.env.OMDB_APIKEY
+}
+
+console.log("OMDB API connected.");
+console.log("-----------------------------------");
--- liri.js
+++ liri.js
@@ -1,21 +1,49 @@
 require('dotenv').config();
+
 var keys = require('./keys.js');
-var Spotify = require('node-spotify-api');
+
+//Axios
 var axios = require('axios');
+
+//Request
+var request = require("request");
 
 //Moment JS
 var moment = require('moment');
+moment().format();
 
 //Spotify Keys 
+var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 
 var fs = require('fs');
 
-
-moment().format();
-
+//Assigning command and input
 var command = process.argv[2];
 var input = process.argv[3];
+
+//Function used to find concerts from queried artist and the date and venue they are playing at.
+function whenConcert() {
+    axios.get("https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp")
+        .then(function (response) {
+            for (var i = 0; i < response.data.length; i++) {
+
+                var datetime = response.data[i].datetime; //Saves datetime response into a variable
+                var dateArr = datetime.split('T'); //Attempting to split the date and time in the response
+
+                var concertResults =
+
+                    "\nVenue Name: " + response.data[i].venue.name +
+                    "\nVenue Location: " + response.data[i].venue.city +
+                    "\nDate of the Event: " + moment(dateArr.datetime).format("MM/DD/YYYY") +
+                    "\n--------------------------------------------------------------------";
+                console.log(concertResults);
+            }
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
 
 //Function: spotify-this-song; Searches Spotify API for whatever is entered into process.arg[3]
 function spotifySong(spotifyQuery) {
@@ -25,7 +53,10 @@ function spotifySong(spotifyQuery) {
         spotifyQuery = "I Want It That Way";
     }
 
-    spotify.search({ type: 'track', query: spotifyQuery }, function (err, data) {
+    spotify.search({
+        type: 'track',
+        query: spotifyQuery
+    }, function (err, data) {
 
         //If there is an error, log error that has happen.
         if (err) {
@@ -55,32 +86,43 @@ function spotifySong(spotifyQuery) {
     });
 }
 
-function whenConcert(){
-    axios.get("https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp")
-    .then(function(response) {    
-        for (var i = 0; i < response.data.length; i++) {
+//Function: movie-this; Looks up movie from OMDB and gives Title, Year, Ratings from IMDB and Rotten Tomatoes, Country, Plot and Actors
+var omdbMovie = function (movie) {
+    var URL = `http://www.omdbapi.com/?apikey=${keys.omdb.apikey}&t=${movie ? movie : "Mr. Nobody"}`;
 
-            var datetime = response.data[i].datetime; //Saves datetime response into a variable
-            var dateArr = datetime.split('T'); //Attempting to split the date and time in the response
+    request(URL, function (err, res, body) {
+        if (err) throw err;
 
-            var concertResults = 
-                
-                    "\nVenue Name: " + response.data[i].venue.name + 
-                    "\nVenue Location: " + response.data[i].venue.city +
-                    "\nDate of the Event: " + moment(dateArr.datetime).format("MM/DD/YYYY") +
-                    "\n--------------------------------------------------------------------";
-            console.log(concertResults);
+        var data = JSON.parse(body);
+
+        if (data && !data.Error) {
+            var output = [
+                "Title: " + data.Title,
+                "Year: " + data.Year,
+                "Ratings:",
+                "IMDB: " + (data.Ratings.length && data.Ratings[0]) ? data.Ratings[0].Value : "N/A",
+                "Rotten Tomatoes: " + (data.Ratings.length && data.Ratings[1]) ? data.Ratings[1].Value : "N/A",
+                "Country: " + data.Country,
+                "Language: " + data.Language,
+                "Plot: " + data.Plot,
+                "Actors: " + data.Actors,
+                "\n-----------------------------------"
+            ].join("\n");
+
+            console.log(output);
+            fs.appendFile("log.txt", output + "\n", (err) => {
+                if (err) throw err;
+            });
+        } else {
+            console.log(`Error: ${data.Error}`);
+            fs.appendFile("log.txt", `Error: ${data.Error}\n`, (err) => {
+                if (err) throw err;
+            });
         }
-    })
-    .catch(function (error) {
-        console.log(error);
     });
-
-
-
 }
 
-//
+//Switch cases for commands
 var ask = function (commands, returnData) {
     switch (commands) {
         //Case to match
@@ -94,7 +136,7 @@ var ask = function (commands, returnData) {
             break;
 
         case "movie-this":
-            movieData(returnData);
+            omdbMovie(returnData);
             break;
 
         case 'do-what-it-says':
@@ -102,13 +144,28 @@ var ask = function (commands, returnData) {
             break;
 
         default:
-            console.log("Invalid command. Please try again and use the following commands:"
-            + "\nconcert-this" //done
-            + "\nspotify-this-song" //done
-            + "\nmovie-this"
-            + "\ndo-what-it-says"
-            + "\n---------------------------------");
+            console.log("Invalid command. Use notionation 'node liri.js [argument] [song/movie name]'. Please try again and use the following commands:" +
+                "\nconcert-this"
+                +
+                "\nspotify-this-song" 
+                +
+                "\nmovie-this" + 
+                "\ndo-what-it-says" + 
+                "\n---------------------------------");
     }
 };
+
+var doWhatItSays = function () {
+    fs.readFile("random.txt", "utf8", function (err, data) {
+        if (err) throw err;
+
+        var cmd = data.split(",")[0];
+        var term = data.split(",")[1];
+
+        ask(cmd, term);
+    });
+};
+
+doWhatItSays();
 
 ask(command, input);
